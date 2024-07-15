3dda1d51f44cbde0591a16167988f387542758c2
filimon4uck <filimon4uck.yura@gmail.com>
2024-07-15 02:04:39 +0200

    added  check isdigit and  added check len in call mean method

--- 1/main.py
+++ 1/main.py
@@ -9,7 +9,7 @@ def total_salary(path):
     try:
         with open(path, mode="r", encoding="utf-8") as file:
             salaries = [int(salary.strip().split(',')[
-                1]) for salary in file]
-            return [sum(salaries), mean(salaries)]
+                1]) for salary in file if salary.strip().split(',')[1].strip().isdigit()]
+            return [sum(salaries), mean(salaries) if len(salaries) > 0 else 0]
     except FileNotFoundError:
         return 'File not found'
